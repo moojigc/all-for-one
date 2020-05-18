afe7897cc9445475c195e6dcd3eac0927e7262fb
Moojig <moojigc@gmail.com>
2020-05-18 19:38:08 -0400

    fixed merging issue

--- public/js/app.js
+++ public/js/app.js
@@ -1,43 +1,35 @@
-$('input[type=file]').on("change", function () {
+$("input[type=file]").on("change", function () {
+	var $files = $(this).get(0).files;
 
-    var $files = $(this).get(0).files;
+	if ($files.length) {
+		// Reject big files
+		if ($files[0].size > $(this).data("max-size") * 1024) {
+			console.log("Please select a smaller file");
+			return false;
+		}
 
-    if ($files.length) {
+		console.log("Upload");
 
-        // Reject big files
-        if ($files[0].size > $(this).data("max-size") * 1024) {
-            console.log("Please select a smaller file");
-            return false;
-        }
+		var settings = {
+			async: false,
+			crossDomain: true,
+			processData: false,
+			contentType: false,
+			type: "POST",
+			url: apiUrl,
+			headers: {
+				Authorization: "e932edc570d9a1f" + apiKey,
+				Accept: "application/json"
+			},
+			mimeType: "multipart/form-data"
+		};
 
+		var formData = new FormData();
+		formData.append("image", $files[0]);
+		settings.data = formData;
 
-        console.log("Upload");
-
-     
-
-        var settings = {
-            async: false,
-            crossDomain: true,
-            processData: false,
-            contentType: false,
-            type: 'POST',
-            url: apiUrl,
-            headers: {
-                Authorization: 'e932edc570d9a1f' + apiKey,
-                Accept: 'application/json'
-            },
-            mimeType: 'multipart/form-data'
-        };
-
-        var formData = new FormData();
-        formData.append("image", $files[0]);
-        settings.data = formData;
-
-
-        $.ajax(settings).done(function (response) {
-            console.log(response);
-        });
-
-    }
+		$.ajax(settings).done(function (response) {
+			console.log(response);
+		});
+	}
 });
-  });
