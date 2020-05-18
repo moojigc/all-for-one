15261922341035fdf5efe299d65d37defe0780f0
Gurprem Bhullar <bhullargurprem@gmail.com>
2020-05-18 14:15:51 -0400

    practice setting up gitignore

--- public/js/app.js
+++ public/js/app.js
@@ -1,14 +1,1 @@
-const fetch = require('node-fetch')
- 
-async function fetchAvatarUrl(userId) {
-    const response = await fetch('https://myurl/${userId}')
-    const user = await response.json()
-
-    return await Promise.all(user.cats.map(async function(catId) {
-        const response = await fetch('https://imgur.com/image/${imageId}')
-        const catData = await response.json()
-        return catData.imageUrl
-    }))
-}
-const result = fetchimages(123)
-result 'http://images.somecdn.com/cat-21.jpg'
+var queryURLBase = "hello world practice commit"
